11546f2df9fc001224ecced9ab1c6d30a5fc31f2
maxime-mrl <maxitrompette@ymail.com>
2023-03-30 12:03:53 +0200

    project caroussel poc

--- script/index.js
+++ script/index.js
@@ -1,5 +1,13 @@
-// fancy card hover animation (from https://www.youtube.com/watch?v=htGfnF1zN4g&list=PLlNMRtjl29p5QBGgLiK4KdqoWu-QJYolB&index=1)
 const cards = document.querySelectorAll(".content");
+const url = window.location.href.split("#");
+
+const images = document.querySelectorAll(".caroussel-content > *");
+const autoDefil = document.querySelector(".auto"); 
+let actualSlide = 0;
+let autoSlideInterval;
+
+/* ----------------------- fancy card hover animation ----------------------- */
+// (from https://www.youtube.com/watch?v=htGfnF1zN4g&list=PLlNMRtjl29p5QBGgLiK4KdqoWu-QJYolB&index=1)
 cards.forEach(card => {
     card.onmousemove = e => { // listen for mous movement for all card
         // extract relative mouse position
@@ -12,8 +20,7 @@ cards.forEach(card => {
     }
 })
 
-// card navigation
-const url = window.location.href.split("#");
+/* ----------------------------- card navigation ---------------------------- */
 function checkUrl() {
     const splitedUrl = url.length === 2 && url[1].length > 1 ? url : false; // check if an anchor is present to navigate to
     if (splitedUrl) {
@@ -34,9 +41,31 @@ function navTo(target) { // return true if succes
     return true;
 }
 
+/* -------------------------------- CAROUSSEL ------------------------------- */
+function changeSlide(to) {
+    images[actualSlide].className = "hidden"; // hide last image
+    actualSlide += to; // define slide to display for access latter images array
+    if (actualSlide < 0) actualSlide = images.length-1;
+    else if (actualSlide >= images.length) actualSlide = 0;
+    images[actualSlide].className = "active"; // display wanted image
+}
+
+function play() {
+    autoSlideInterval = setInterval(() => {
+        changeSlide(1)
+    }, 2000);
+    autoDefil.setAttribute("data-state", "play") // change datastae of play/pause container to update CSS
+}
+function pause() {
+    clearInterval(autoSlideInterval);
+    autoDefil.setAttribute("data-state", "pause") // change datastae of play/pause container to update CSS
+}
+
+
+/* ------------------------------ Check scroll ------------------------------ */
 document.addEventListener("scroll", () => { // make sure that it's on top (sometimes on ios embeded browser scroll was possible during load then blocked)
     window.scroll(0,0);
 })
 setTimeout(() => { // same as for the addeventListener
     window.scroll(0,0);
-}, 1000)
+}, 1000);
